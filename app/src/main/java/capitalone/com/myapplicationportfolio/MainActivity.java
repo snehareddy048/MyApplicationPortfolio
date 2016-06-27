138bdbe8b0b5a20d7881cfa279558d11bbc37d43
sneha.yeluguri@capitalone.com <sneha.yeluguri@capitalone.com>
2016-06-27 15:37:33 -0400

    added links to projects

--- app/src/main/java/capitalone/com/myapplicationportfolio/MainActivity.java
+++ app/src/main/java/capitalone/com/myapplicationportfolio/MainActivity.java
@@ -1,5 +1,7 @@
 package capitalone.com.myapplicationportfolio;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -8,6 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
     private static String displayMessage = "This button will launch ";
+    ComponentName componentName;
+    Intent intent = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +19,50 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void spoifyStreamer(View v) {
-        Toast.makeText(getApplicationContext(), displayMessage + "spotify streamer!", Toast.LENGTH_SHORT).show();
+    public void moviesApp(View v) {
+        String pkg = "com.example.snehaanand.";
+        componentName = new ComponentName(pkg + "moviesapp",
+                pkg + "moviesapp.view.MainActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
     }
 
     public void scoresApp(View v) {
-        Toast.makeText(getApplicationContext(), displayMessage + "scores app!", Toast.LENGTH_SHORT).show();
-    }
+        String pkg = "barqsoft.footballscores";
+        componentName = new ComponentName(pkg ,
+                pkg + ".MainActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
+            }
 
     public void libraryApp(View v) {
-        Toast.makeText(getApplicationContext(), displayMessage + "library app!", Toast.LENGTH_SHORT).show();
-    }
+        String pkg = "it.jaschke.alexandria";
+        componentName = new ComponentName(pkg ,
+                pkg + ".MainActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
+            }
 
     public void buildItBigger(View v) {
         Toast.makeText(getApplicationContext(), displayMessage + "build it bigger app!", Toast.LENGTH_SHORT).show();
     }
 
-    public void xyzReader(View v) {
-        Toast.makeText(getApplicationContext(), displayMessage + "xyz reader app!", Toast.LENGTH_SHORT).show();
-    }
+    public void materialApp(View v) {
+        String pkg = "com.example.xyzreader";
+        componentName = new ComponentName(pkg ,
+                pkg + ".ui.ArticleListActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
+            }
+
+    public void goUbiquitous(View v) {
+        String pkg = "com.example.snehaanandyeluguri.ubiquitouswatchface";
+        componentName = new ComponentName(pkg ,
+                pkg + ".MainActivity");
+        intent.setComponent(componentName);
+        startActivity(intent);
+            }
+
 
     public void capstoneMyOwnApp(View v) {
         Toast.makeText(getApplicationContext(), displayMessage + "my capstone app!", Toast.LENGTH_SHORT).show();
